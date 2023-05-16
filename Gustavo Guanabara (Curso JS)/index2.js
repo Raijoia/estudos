555143f4ebfccb53e892e2712921045bba66d8e7
Raí <megac154@gmail.com>
2023-05-15 22:04:28 -0300

    anotação

--- Gustavo Guanabara (Curso JS)/index2.js
+++ Gustavo Guanabara (Curso JS)/index2.js
@@ -1,4 +1,5 @@
 // condição aninhada 
+// para pegar a hora do sistema criei uma variavel com nome agora para pegar o dia com new Data(), depois criei uma variavel com nome hora para pegar a hora com o comando agora.getHours()
 var agora = new Date()
 var hora = agora.getHours()
 console.log(`Agora são exatamente ${hora} horas`)
