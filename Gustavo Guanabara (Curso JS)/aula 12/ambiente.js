b47032aa22dd3dd3dd7ef67a2510d26028acbe18
Raí <megac154@gmail.com>
2023-05-22 12:31:21 -0300

    repetições while e do/while

---
+++ Gustavo Guanabara (Curso JS)/aula 12/ambiente.js
@@ -0,0 +1,18 @@
+// repetições com teste lógico no inicio while(condição)       while=enquanto
+/*
+var c = 1
+while (c <= 6) {
+  console.log(`Passo ${c}`)
+  c++
+}
+*/
+
+// repetições com teste lógico no final do{}while(condição)
+
+var c = 1
+do{
+  console.log(`Passo ${c}`)
+  c++
+} while (c <= 6)
+
+//console.log('Tudo bem?')
