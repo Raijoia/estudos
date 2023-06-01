109fb4af99fbd0d57cf01625f6a6193f491f0d18
Raí <megac154@gmail.com>
2023-06-01 00:36:42 -0300

    object

---
+++ study(JS)/object/aula 16/ambiente.js
@@ -0,0 +1,13 @@
+// object, mais fácil de identificar caso haja nome e números em uma array, podendo criar até uma função dentro de um object
+
+// isso é uma array
+let num = [5, 8, 4]
+
+// isso é um object
+let amigo = { // após cada valor do objeto, é necessário que coloque vírgula para conseguir colocar mais um objeto e valor
+  nome:'josé',
+  sexo:'M',
+  peso: 84.4,
+  engordar(p){}
+  
+}
